0989e909fecdf2dd46a3a06817d8fe92f1f817b4
mcountryman <countryman.marco@gmail.com>
2021-10-18 15:51:31 -0400

    changes

--- enumerables_project/enumerables.rb
+++ enumerables_project/enumerables.rb
@@ -1,3 +1,5 @@
+
+require "byebug"
 class Array 
 
     def my_each(&prc)
@@ -17,6 +19,52 @@ class Array
         results_array
     end
 
-    p "Hello Worlf"
+    def my_reject(&prc)
+        results_array = []
+        self.my_each do |ele|
+            results_array << ele if !prc.call(ele)
+        end
+        results_array
+    end
 
+    def my_any?(&prc)
+        self.my_each do |ele|
+            return true if prc.call(ele)
+        end
+
+        false
+    end
+
+     def my_all?(&prc)
+        self.my_each do |ele|
+            return false if !prc.call(ele)
+        end
+
+        true
+    end
+
+    def my_flatten
+
+        #return [ele] if !ele.is_a?(Array)
+        #[1,2,[3]]
+        flattened = []
+        self.each.with_index do |ele, i|
+          
+            if !ele.is_a?(Array)
+                flattened << ele  
+            else   
+                  #debugger
+                flattened += ele.my_flatten
+            end
+
+        end
+
+
+        flattened
+    end
+
+    #[1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
 end
+
+ p [1,2,[3]].my_flatten
+ p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
